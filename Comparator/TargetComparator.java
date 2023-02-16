faed0e37c219b16459c624d3c4544981f9ef7075
gunaskr <a.guna14@gmail.com>
2023-02-16 12:52:46 +0530

    Update TargetComparator.java

--- Comparator/TargetComparator.java
+++ Comparator/TargetComparator.java
@@ -1,1 +1,17 @@
 
+public class TargetComparator {
+public static void main(String... args){
+ 	 List sample = new ArrayList<Employee>();
+ 	 sample.add(new Employee(20));
+ 	 sample.add(new Employee(30));
+ 	 sample.add(new Employee(10));
+ 	 
+ 	 Collections.sort(sample);
+ 	 System.out.println("Ascending with comparable");
+ 	 System.out.println(sample);
+ 	 System.out.println("Descending with comparator");
+ 	 Collections.sort(sample,(Employee a,Employee b)->b.age-a.age);
+ 	 System.out.println(sample);
+ 
+ }
+}
